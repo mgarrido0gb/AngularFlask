58ebafa0e30b38b3f645d79e46f4a1eed3cdacd2
mgarrido0gb <contacto.garridomatias@gmail.com>
2022-11-03 19:22:52 -0400

    Pequeño cambio en service

--- src/app/components/procesos/usuarios.service.ts
+++ src/app/components/procesos/usuarios.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs'
 
-import { HttpClient } from "@angular/common/http";
+import { HttpClient ,HttpHeaders } from "@angular/common/http";
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 
 
 export class UsuariosServices
@@ -14,6 +16,6 @@ export class UsuariosServices
 
     //metodo que lista endpoint
     public getUsuarios():Observable<any>{
-        return this.http.get(this.API_SERVER)
+        return this.http.get<any>(this.API_SERVER);
     }
 }
